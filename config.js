79005c79ec51067235b02af5d0eac0c7e3410c58
albacarechecklist <drsms2040@gmail.com>
2026-09-05 20:47:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.ALBA_CONFIG = {
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseAnonKey: 'YOUR_SUPABASE_PUBLISHABLE_ANON_KEY',
+  supabaseUrl: 'https://mggyhlqnfimtzupkbcfa.supabase.co',
+  supabaseAnonKey: 'sb_publishable_4rszDBuQgDa8gXf1z7AoXg_GvLgzcpl',
   appName: 'Alba Care Daily Checklist'
 };
